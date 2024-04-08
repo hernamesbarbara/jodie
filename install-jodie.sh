b14323e130de6bd40760b3a22e16ef5718351e57
austin <tips@cia.lol>
2024-04-08 14:22:47 -0400

    install with pyinstaller

---
+++ install-jodie.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+echo "Installing jodie with \`pyinstaller/install-jodie.sh\`"
+echo
+
+pyinstaller --name jodie --onefile jodie/__main__.py
+echo "done"
+echo
+
+echo "Signing code with Apple entitlements for macOS Contacts..."
+codesign \
+    --entitlements jodie.entitlements \
+    --sign "$APPLE_DEVELOPER_ID_APPLICATION" \
+    --options runtime \
+    --force \
+    dist/jodie
+
+# codesign --remove-signature dist/jodie
+# codesign -dv --verbose=4 dist/jodie
