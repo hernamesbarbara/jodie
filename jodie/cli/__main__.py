5c0740e1039bd13a906c361475a462e9143f6c54
austin <tips@cia.lol>
2024-05-22 14:30:56 -0400

    comment out note

--- jodie/cli/__main__.py
+++ jodie/cli/__main__.py
@@ -2,8 +2,40 @@
 # jodie/cli/__main__.py
 import sys
 from docopt import docopt
-from .__doc__ import __doc__
+# from .__doc__ import __doc__
 import jodie
+
+__doc__ = """jodie - Manage macOS Contacts.app from command line!
+
+Usage: 
+    jodie new [EMAIL NAME COMPANY TITLE NOTE...]
+    jodie new [options]
+    jodie parse [options] TEXT
+
+Arguments:
+    EMAIL                               Email address for the contact you want to create.
+    NAME                                Full name for the contact you want to create.
+    COMPANY                             Company name.
+    TITLE                               Job title.
+    NOTE                                Any text you want to save in the `Note` field in Contacts.app.
+    TEXT                                Text for jodie to try her best to parse semi-intelligently if she can.
+
+Options:
+    -C COMPANY --company=COMPANY        Company name.
+    -E EMAIL --email=EMAIL              Email.
+    -F FIRST --first=FIRST              First name.
+    -L LAST --last=LAST                 Last name.
+    -U NAME --full-name=NAME            Full name.
+    -N NOTE --note=NOTE                 Any text you want to save in the `Note` field in Contacts.app.
+    -P PHONE --phone=PHONE              Phone.
+    -T TITLE --title=TITLE              Job title.
+    -X TEXT  --text=TEXT                Text for jodie to try her best to parse semi-intelligently if she can.    
+    -W WEBSITE --website=WEBSITE        Website / URL.
+    -H --help                           Show this screen.
+    -V --version                        Show version.
+
+"""
+
 
 COMMANDS = ('new', 'parse',)
 NOT_ARGS = ('--help', '--version',)
@@ -74,6 +106,7 @@ def main():
         job_title=title,
         company=company,
         website=website
+        # note=note
     )
 
     sys.stdout.write(f'Saving...\n{c}\n')
